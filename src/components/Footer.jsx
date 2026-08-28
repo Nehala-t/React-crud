b956d45c2519fc21b2acb5fb5a9cb9c42b45da15
nehala-t <nehalamehak2772@gmail.com>
2026-08-28 11:12:54 +0530

    update footer social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => {
         <Link to="/contact" className='menu-item-1'>Contact</Link>
       </div>
       <div className='social-meadia'>
-        <div><img src="/images/iconoir_facebook.png" alt="" /></div>
-        <div><img src="/images/basil_twitter-outline.png" alt="" /></div>
-        <div><img src="/images/Vector (10).png" alt="" /></div>
+        <Link to="https://www.facebook.com/"><img src="/images/iconoir_facebook.png" alt="" /></Link>
+        <Link to="https://x.com/"><img src="/images/basil_twitter-outline.png" alt="" /></Link>
+        <Link to="https://www.instagram.com/"><img src="/images/Vector (10).png" alt="" /></Link>
       </div>
       <div><p>© 2025 MyApp. All Rights Reserved.</p></div>
 
